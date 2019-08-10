8a561acaaa336425f22716650f163cfba1bb3a69
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-08-10 08:29:36 -0700

    reviews can now be added to products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,25 @@
 class ReviewsController < ApplicationController
 
   def new
-    @product = Product.find(params[:id])
-    @review = @Product.reviews.new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
     render :new
   end
 
-  
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(id: params[:product_id])
+    else
+      render :new
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :rating, :content_body)
+    end
+
+
 end
