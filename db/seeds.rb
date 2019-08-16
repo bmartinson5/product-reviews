9ef74aa873245e9504489c54f01ed399fff5a65b
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-08-16 08:06:28 -0700

    Added faker seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+
+50.times do
+  Product.create!(name: Faker::Food.ingredient,
+    cost: Faker::Commerce.price(range: 0..20.00),
+    country_of_origin: Faker::Address.country)
+end
+200.times do
+  product_id = Product.all.sample.id
+  Review.create!(product_id: product_id,
+    author: Faker::Name.name,
+    content_body: Faker::Lorem.sentence(word_count: 15, supplemental: false, random_words_to_add: 0),
+    rating: Faker::Number.between(from: 1, to: 5))
+end
